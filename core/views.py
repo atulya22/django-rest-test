809a6fbad40efe57cfb38448e01f67b86d393631
atulya22 <atulya.shetty@hotmail.com>
2020-06-01 22:32:00 -0400

    Use generics to test DRF

--- core/views.py
+++ core/views.py
@@ -1,27 +1,51 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
+from rest_framework import mixins
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+
 from .serializers import PostSerializer
 from .models import Post
 
-class TestView(APIView):
 
-    permission_classes = (IsAuthenticated, )
+class PostView(
+    mixins.ListModelMixin ,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
     def get(self, request, *args, **kwargs):
-        query = Post.objects.all()
-        post = query.first()
-        # serializer =PostSerializer(query, many=True)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+        return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+        return self.create(request, *args, **kwargs)
 
-        return Response(serializer.errors)
+
+class PostCreateView(generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+
+# class TestView(APIView):
+#
+#     permission_classes = (IsAuthenticated, )
+#
+#     def get(self, request, *args, **kwargs):
+#         query = Post.objects.all()
+#         post = query.first()
+#         # serializer =PostSerializer(query, many=True)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#
+#         return Response(serializer.errors)
